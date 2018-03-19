3ccae6f1b8502a4546df436fbc5ccc7a17e73aaa
Mahir92 <asefkabir92@gmail.com>
2018-03-19 09:19:41 +0600

    Print radius and center of opposite surface

--- Program.cpp
+++ Program.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 #define PI 2 * acos(0.0)
 
-struct node {
+struct Node {
     double x;
     double y;
     double z;
@@ -18,19 +18,19 @@ struct node {
     int green;
     int blue;
 
-    node() {
+    Node() {
         x = y = z = 0.0;
         red = green = blue = 0;
     }
 
-    node(double _x, double _y, double _z) {
+    Node(double _x, double _y, double _z) {
         x = _x;
         y = _y;
         z = _z;
         red = green = blue = 0;
     }
 
-    node(double _x, double _y, double _z, int _red, int _green, int _blue) {
+    Node(double _x, double _y, double _z, int _red, int _green, int _blue) {
         x = _x;
         y = _y;
         z = _z;
@@ -46,21 +46,21 @@ struct node {
     }
 };
 
-struct cylinder {
-    node center;
+struct Cylinder {
+    Node center;
     double height;
     double diameter;
-    node surfaceCenter;
+    Node surfaceCenter;
+    Node oppositeSurfaceCenter;
 
-    cylinder(node _center, double _height, double _diameter, node _anotherPointOnCenterSurface) {
+    Cylinder(Node _center, double _height, double _diameter, Node _surfaceCenter, Node _oppSurfaceCenter) {
         center = _center;
         height = _height;
         diameter  = _diameter;
-        surfaceCenter = _anotherPointOnCenterSurface;
+        surfaceCenter = _surfaceCenter;
+        oppositeSurfaceCenter = _oppSurfaceCenter;
     }
 };
-
-vector<node> nodes;
 
 vector<double> Split(string str) {
     vector<double> ret;
@@ -81,58 +81,62 @@ int doubleToInt(double d) {
     return (int)d;
 }
 
-node GetNode(string str) {
+Node GetNode(string str) {
     int n = str.length();
 
     vector<double> props = Split(str);
 
     return (props.size() == 6)
-        ? node(props[0], props[1], props[2], doubleToInt(props[3]), doubleToInt(props[4]), doubleToInt(props[5]))
-        : node(0.0, 0.0, 0.0, 0, 0, 0);
+        ? Node(props[0], props[1], props[2], doubleToInt(props[3]), doubleToInt(props[4]), doubleToInt(props[5]))
+        : Node(0.0, 0.0, 0.0, 0, 0, 0);
 }
 
 // Idea: First create a bounding box then wrap up that box within a cylinder
-cylinder GetBoundingCylinder(vector<node> nodes) {
-    node cornerMax(INT_MIN, INT_MIN, INT_MIN);
-    node cornerMin(INT_MAX, INT_MAX, INT_MAX);
+Cylinder GetBoundingCylinder(vector<Node> nodes) {
+
+    Node cornerMax(INT_MIN, INT_MIN, INT_MIN);
+    Node cornerMin(INT_MAX, INT_MAX, INT_MAX);
 
     int n = nodes.size();
 
     for(int i = 0; i < n; ++i) {
-        cornerMax = node(max(cornerMax.x, nodes[i].x), max(cornerMax.y, nodes[i].y), max(cornerMax.z, nodes[i].z));
-        cornerMin = node(min(cornerMin.x, nodes[i].x), min(cornerMin.y, nodes[i].y), min(cornerMin.z, nodes[i].z));
+        cornerMax = Node(max(cornerMax.x, nodes[i].x), max(cornerMax.y, nodes[i].y), max(cornerMax.z, nodes[i].z));
+        cornerMin = Node(min(cornerMin.x, nodes[i].x), min(cornerMin.y, nodes[i].y), min(cornerMin.z, nodes[i].z));
     }
 
     //Corner points of bounding box
-    node a (cornerMin.x, cornerMin.y, cornerMin.z);
-    node b (cornerMin.x, cornerMin.y, cornerMax.z);
-    node c (cornerMax.x, cornerMin.y, cornerMax.z);
-    node d (cornerMax.x, cornerMin.y, cornerMin.z);
-    node e (cornerMax.x, cornerMax.y, cornerMax.z);
-    node f (cornerMin.x, cornerMax.y, cornerMax.z);
-    node g (cornerMin.x, cornerMax.y, cornerMin.z);
-    node h (cornerMax.x, cornerMax.y, cornerMin.z);
+    Node a(cornerMin.x, cornerMin.y, cornerMin.z);
+    Node b(cornerMin.x, cornerMin.y, cornerMax.z);
+    Node c(cornerMax.x, cornerMin.y, cornerMax.z);
+    Node d(cornerMax.x, cornerMin.y, cornerMin.z);
+    Node e(cornerMax.x, cornerMax.y, cornerMax.z);
+    Node f(cornerMin.x, cornerMax.y, cornerMax.z);
+    Node g(cornerMin.x, cornerMax.y, cornerMin.z);
+    Node h(cornerMax.x, cornerMax.y, cornerMin.z);
 
-    // Cylinder axis line going through centerPointOfCylinder and centerOfAPlane point
-    node centerPointOfCylinder ((cornerMax.x + cornerMin.x) / 2, (cornerMax.y + cornerMin.y) / 2, (cornerMax.z + cornerMin.z) / 2);
-    node centerPointOfAPlane ((e.x + g.x) / 2, (e.y + g.y) / 2, (e.z + g.z) / 2);
+    //Cylinder axis line going through centerPointOfCylinder and centerOfAPlane point
+    Node centerPointOfCylinder((cornerMax.x + cornerMin.x) / 2, (cornerMax.y + cornerMin.y) / 2, (cornerMax.z + cornerMin.z) / 2);
+    Node centerPointOfAPlane((e.x + g.x) / 2, (e.y + g.y) / 2, (e.z + g.z) / 2);
+    Node centerPointOfOppPlane((a.x + c.x) / 2, (a.y + c.y) / 2, (a.z + c.z) / 2);
 
-    //node centerPointOfAPlane2 ((a.x + c.x) / 2, (a.y + c.y) / 2, (a.z + c.z) / 2);
     double diameter = max(sqrt((a.x - c.x)*(a.x - c.x) + (a.y - c.y)*(a.y - c.y) + (a.z - c.z)*(a.z - c.z)),
                           sqrt((b.x - d.x)*(b.x - d.x) + (b.y - d.y)*(b.y - d.y) + (b.z - d.z)*(b.z - d.z))); // take max diagonals form plane
 
     double height = fabs(cornerMax.y - cornerMin.y);
 
-    return cylinder(centerPointOfCylinder, height, diameter, centerPointOfAPlane);
+    return Cylinder(centerPointOfCylinder, height, diameter, centerPointOfAPlane, centerPointOfOppPlane);
 }
 
 int main() {
+
     ifstream fin("cmesh0.ply");
     ofstream fout("output.txt");
 
     string str;
 
     bool header = true;
+
+    vector<Node> nodes;
 
     while(getline(fin, str)) {
         if(header) {
@@ -145,11 +149,11 @@ int main() {
         }
     }
 
-    int n = nodes.size();
-
-    cylinder ans = GetBoundingCylinder(nodes);
+    Cylinder ans = GetBoundingCylinder(nodes);
 
     fout << "Cylinder center: " << ans.center.print() << endl;
     fout << "Cylinder height: " << ans.height << endl;
+    fout << "Cylinder radius: " << ans.diameter / 2 << endl;
     fout << "Center of a surface: " << ans.surfaceCenter.print() << endl;
+    fout << "Center of a opposite surface: " << ans.oppositeSurfaceCenter.print() << endl;
 }
